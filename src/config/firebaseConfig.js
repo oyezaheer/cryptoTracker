01ca86c49c48541ded7f90cff577a62c3b1d9b34
oyezaheer <zaheer286khan@gmail.com>
2024-06-20 10:00:34 +0530

    Initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9KACuhbU1bDCyRx7zQ7EJ-eo_CkcGGuA",
+  authDomain: "crypto-hunter-45df3.firebaseapp.com",
+  projectId: "crypto-hunter-45df3",
+  storageBucket: "crypto-hunter-45df3.appspot.com",
+  messagingSenderId: "755526594158",
+  appId: "1:755526594158:web:5366893b442b96c65a407f",
+  measurementId: "G-PE1RKSJXQ1"
+};
+
+export default firebaseConfig;
+
+// project-755526594158
+
